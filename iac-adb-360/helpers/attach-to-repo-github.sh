aee7e724bfe28458493cb8ecadce0ec4fe59ee8b
iamjenetzler <jen.etzler@gmail.com>
2025-10-13 16:18:24 -0700

    i

--- iac-adb-360/helpers/attach-to-repo-github.sh
+++ iac-adb-360/helpers/attach-to-repo-github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-resourcegroupname=$1 #'rg-wus3-adbmidp0912-dev'
+resourcegroupname=$1 #'rg-wus3-adbmidp0912-prd'
 echo "rgname : $resourcegroupname as parm 1"
 tenantid=$2 #'12ce7121-18c7-4841-98f9-3b26fc8af34f'
 echo "tenant : $tenantid as parm 2"
